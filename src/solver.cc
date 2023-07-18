faf3a4929a397791cf75b347262d1689dc0a2977
Maks Verver <maks@verver.ch>
2023-07-18 16:44:30 +0200

    Support solving multiple states in a single execution.

--- src/solver.cc
+++ src/solver.cc
@@ -1,27 +1,43 @@
 #include "state.h"
 
-int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    std::cerr << "Usage: solver <state>" << std::endl;
-    return 1;
-  }
+#include <iostream>
+#include <cstring>
+
+bool Solve(const char *desc) {
   State state;
-  const char *desc = argv[1];
   for (int i = 0; i < 81; ++i) {
     if (desc[i] >= '1' && desc[i] <= '9') {
       Move move = {i, desc[i] - '0'};
       if (!state.CanPlay(move)) {
         std::cerr << "Invalid digit as index " << i << std::endl;
-        return 1;
+        return false;
       }
       state.Play(move);
     } else if (desc[i] != '.' && desc[i] != '0') {
       std::cerr << "Invalid character at index " << i << std::endl;
-      return 1;
+      return false;
     }
   }
   CountState cs = state.CountSolutions(1000);
   assert(!cs.WorkLimitExceeded());
   if (cs.CountLimitReached()) std::cout << "At least ";
   std::cout << cs.count << " solutions" << std::endl;
+  return true;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cerr << "Usage:\n"
+        "\tsolver <state>  (solves a single state)\n"
+        "\tsolver -        (solve states read from standard input)\n";
+    return 1;
+  }
+  if (strcmp(argv[1], "-") == 0) {
+    std::string line;
+    while (std::getline(std::cin, line)) {
+      if (!Solve(line.c_str())) return 1;
+    }
+  } else {
+    if (!Solve(argv[1])) return 1;
+  }
 }
