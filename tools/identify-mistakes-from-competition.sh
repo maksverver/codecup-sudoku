7ffbd1c85db5cbac0cdc34c4d49e64c0c5d61943
Maks Verver <maks@verver.ch>
2023-08-28 02:10:09 +0200

    Add scripts to detect mistakes during competition.

---
+++ tools/identify-mistakes-from-competition.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Calculates mistakes in a competition.
+#
+# Prints line of the form:
+#
+# <game_id> <1-based turn> <user name>
+
+basedir=$(dirname $(readlink -f "$0"))
+cd "${basedir}"/..
+
+if [ $# != 1 ]; then
+  echo "Usage: $0 <competition-id>"
+  exit 1
+fi
+
+competition_id=$1
+competition_csv=competition-results/${competition_id}-games.csv
+
+if [ ! -f "$competition_csv" ]; then
+  echo "Missing input file: $competition_csv"
+  exit 1
+fi
+
+grep '!' "$competition_csv" | while IFS=, read game round is_swiss user1 score1 status1 user2 score2 status2 moves solution
+do
+  echo $game $user1 $status1 $user2 $status2 $moves >&2
+  mistakes=$(tools/identify-mistakes.sh "$moves")
+  for turn in $mistakes; do
+    if [ $(($turn % 2)) -eq 1 ]; then
+      failed_user=${user1}
+    else
+      failed_user=${user2}
+    fi
+    printf "%s\t%d\t%s\n" "$game" "$turn" "$failed_user"
+  done
+done
---
+++ tools/identify-mistakes.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# This tool uses the solver to identify mistakes from a game transcript.
+#
+# It prints the incorrect move numbers to stdout (one per line), from end to
+# beginning.
+#
+# Odd numbers indicate a mistake by player 1.
+# Even numbers indicate a mistake by player 2.
+#
+
+set -e -E -o pipefail
+
+# The max_work parameter determines how deeply the game is analyzed.
+# When the work limit is exceeded, analysis is stopped, and further mistakes
+# are not identified.
+SOLVER='output/release/solver --analyze_max_work=1e9'
+
+basedir=$(dirname $(readlink -f "$0"))
+cd "${basedir}"/..
+
+moves=$1
+
+if [ $# != 1 ]; then
+  echo "Usage: $0 <transcript>"
+fi
+
+if ! echo "$moves" | grep -E -q '^([A-I][a-i][1-9][^[:alnum:]])*[A-I][a-i][1-9][!]$'; then
+  echo "Invalid transcript!"
+  exit 1
+fi
+
+if ! $SOLVER "$moves" | grep -qF 'Solution is unique!'; then
+  echo "Transcript does not end with unique solution!"
+  exit 1
+fi
+
+winning=1
+while true; do
+  moves=${moves::-4}
+  turn=$((${#moves} / 4))  # 0-based!
+  result=$($SOLVER "$moves" | grep -E 'Outcome|Analysis incomplete')
+  if [ "$result" = "Analysis incomplete!" ]; then
+    echo "$result" >&2
+    exit 0
+  else
+    if echo "$result" | grep -Fq 'Outcome: WIN'; then
+      outcome=1
+    else
+      outcome=0
+    fi
+    if [ $outcome != $winning ]; then
+      echo "$((turn + 1))"
+      echo "Player $((turn % 2 + 1)) blundered on move $((turn + 1))" >&2
+    fi
+    winning=$((1 - $outcome))
+  fi
+done
