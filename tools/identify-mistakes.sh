5289e1924ba28f549e1f5a5ec187566a64d37f79
Maks Verver <maks@verver.ch>
2023-11-03 05:58:47 +0100

    Fix script to identify mistakes, and include incorrect move in output.

--- tools/identify-mistakes.sh
+++ tools/identify-mistakes.sh
@@ -14,7 +14,7 @@ set -e -E -o pipefail
 # The max_work parameter determines how deeply the game is analyzed.
 # When the work limit is exceeded, analysis is stopped, and further mistakes
 # are not identified.
-SOLVER='output/release/solver --analyze_max_work=1e9'
+SOLVER='output/release/solver --analyze-max-work=1e9'
 
 basedir=$(dirname $(readlink -f "$0"))
 cd "${basedir}"/..
@@ -30,23 +30,20 @@ if ! echo "$moves" | grep -E -q '^([A-I][a-i][1-9][^[:alnum:]])*([A-I][a-i][1-9]
   exit 1
 fi
 
-if [ $(( ${#moves} % 4 )) -eq 1 ]; then
-  # Transcript ends with "!" to claim solution is already unique
-  moves=${moves::-1}
-  turn=$((${#moves} / 4))  # 0-based!
-  echo "Player $(( (turn - 1) % 2 + 1)) blundered on move $((turn))" >&2
-fi
+moves=($(echo "$moves" | grep -E -o '[A-I][a-i][1-9]'))
 
-if ! $SOLVER "$moves" | grep -qF 'Solution is unique!'; then
+if ! $SOLVER "${moves[*]}" | grep -qF 'Solution is unique!'; then
   echo "Transcript does not end with unique solution!" >&2
   exit 1
 fi
 
+turn=${#moves[@]}  # 0-based
 winning=1
 while true; do
-  moves=${moves::-4}
-  turn=$((${#moves} / 4))  # 0-based!
-  result=$($SOLVER "$moves" | grep -E 'Outcome|Analysis incomplete|Solution is unique')
+  turn=$((turn - 1))
+  last_move=${moves[$turn]}
+  moves=(${moves[@]:0:$turn})
+  result=$($SOLVER "${moves[*]}" | grep -E 'Outcome|Analysis incomplete|Solution is unique')
   if [ "$result" = "Analysis incomplete!" ]; then
     echo "$result" >&2
     exit 0
@@ -59,7 +56,7 @@ while true; do
       outcome=0
     fi
     if [ $outcome != $winning ]; then
-      echo "Player $((turn % 2 + 1)) blundered on move $((turn + 1))" >&2
+      echo "Player $((turn % 2 + 1)) blundered on move $((turn + 1)) ($last_move)" >&2
     fi
     winning=$((1 - $outcome))
   fi
